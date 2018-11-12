14d6ac0f884455c944f798510b9e63d1b47764aa
neera <neerajathalye.1996@gmail.com>
2018-11-12 20:26:29 +0000

    Completed Indexing for Financial Times Fixed formatting errors in a few documents

--- src/main/java/tcd/ie/Index.java
+++ src/main/java/tcd/ie/Index.java
@@ -16,10 +16,7 @@ import org.apache.lucene.search.similarities.ClassicSimilarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
@@ -33,58 +30,12 @@ public class Index {
     private static String CORPUS_DIRECTORY = "corpus";
 
     public static void main(String[] args) throws IOException {
-        // Analyzer that is used to process TextField
-        Analyzer analyzer = new StandardAnalyzer();
-
-        // To store an index in memory
-        // Directory directory = new RAMDirectory();
-        // To store an index on disk
-        Directory directory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
-        IndexWriterConfig config = new IndexWriterConfig(analyzer);
-
-        config.setSimilarity(new BM25Similarity());
-
-//        System.out.println("Enter 1 for default\nEnter 2 for Classic\nEnter 3 for BM25\n");
-        Scanner in = new Scanner(System.in);
-//        int choice  = in.nextInt();
-
-//        switch (choice)
-//        {
-//            case 1:
-//                do nothing
-//                break;
-//            case 2:
-//                config.setSimilarity(new ClassicSimilarity());
-//                break;
-//            case 3:
-//                config.setSimilarity(new BM25Similarity());
-//                break;
-//        }
-
-
-
-
-        // Index opening mode
-        // IndexWriterConfig.OpenMode.CREATE = create a new index
-        // IndexWriterConfig.OpenMode.APPEND = open an existing index
-        // IndexWriterConfig.OpenMode.CREATE_OR_APPEND = create an index if it
-        // does not exist, otherwise it opens it
-        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-
-        IndexWriter iWriter = new IndexWriter(directory, config);
-
-
-//        File file = new File(String.valueOf(Paths.get(CORPUS_DIRECTORY + "/cran.all.1400")));
 
         File ftDirectory = new File(String.valueOf(Paths.get(CORPUS_DIRECTORY + "/ft")));
         File fr94Directory = new File(String.valueOf(Paths.get(CORPUS_DIRECTORY + "/fr94")));
         File fbisDirectory = new File(String.valueOf(Paths.get(CORPUS_DIRECTORY + "/fbis")));
         File latimesDirectory = new File(String.valueOf(Paths.get(CORPUS_DIRECTORY + "/latimes")));
 
-
-//        File[] files = ftDirectory.listFiles();
-//        for(File f : files)
-//            System.out.println(f.getName());
         ArrayList<File> ftList = addFilesToList(ftDirectory);
         ArrayList<File> fr94List = addFilesToList(fr94Directory);
         ArrayList<File> fbisList = addFilesToList(fbisDirectory);
@@ -99,89 +50,104 @@ public class Index {
 
         latimesList.removeIf(e->e.getName().contains(".txt")); //removes files with .txt extension(readchg and readmela)
 
+        parseFT(ftList);
 
-
-        for(File f : latimesList)
-            System.out.println(f.getName());
-
-
-
-
-
-//        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-//
-//        String line;
-//
-//        int docCount = 0;
-//
-//
-//        StringBuilder documentContents = new StringBuilder();
-//        ArrayList<StringBuilder> documentList = new ArrayList<>();
-//        int documentIndex = 1;
-//
-//
-//        HashMap<String, HashMap<String, String>> docmentsMap = new HashMap<>();
-//
-//        while ((line = bufferedReader.readLine()) != null)
-//        {
-//            if(line.equals(".I 1"))
-//                continue;
-//            if (!line.contains(".I " + (documentIndex + 1)))
-//            {
-//
-//                documentContents.append(line).append("\n");
-//            }
-//            else
-//            {
-//                docCount++;
-//
-//                documentList.add(documentContents);
-//
-//                String T, A, B, W;
-//
-//                T = documentContents.substring((documentContents.indexOf(".T")+3), documentContents.indexOf(".A"));
-//                A = documentContents.substring((documentContents.indexOf(".A")+3), documentContents.indexOf(".B"));
-//                B = documentContents.substring((documentContents.indexOf(".B")+3), documentContents.indexOf(".W"));
-//                W = documentContents.substring((documentContents.indexOf(".W")+3), documentContents.length());
-//
-//
-//                Creating the document
-//                Document doc = new Document();
-//                doc.add(new StringField("document-number", String.valueOf(docCount), Field.Store.YES));
-//                doc.add(new TextField("title", T, Field.Store.YES));
-//                doc.add(new TextField("author", A, Field.Store.YES));
-//                doc.add(new TextField("source", B, Field.Store.YES));
-//                doc.add(new TextField("content", W, Field.Store.YES));
-//
-                //Save the document to the index
-//                iWriter.addDocument(doc);
-//
-//
-//                documentIndex++;
-//                documentContents = new StringBuilder();
-
-//            }
-//        }
 //
 //        System.out.println("Document Count: " + docCount);
 //
+
+    }
+
+    private static void parseFT(ArrayList<File> ftList) throws IOException {
+
+        // Analyzer that is used to process TextField
+        Analyzer analyzer = new StandardAnalyzer();
+
+        // To store an index in memory
+        // Directory directory = new RAMDirectory();
+        // To store an index on disk
+        Directory directory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+
+        config.setSimilarity(new BM25Similarity());
+        // Index opening mode
+        // IndexWriterConfig.OpenMode.CREATE = create a new index
+        // IndexWriterConfig.OpenMode.APPEND = open an existing index
+        // IndexWriterConfig.OpenMode.CREATE_OR_APPEND = create an index if it
+        // does not exist, otherwise it opens it
+        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+
+        IndexWriter iWriter = new IndexWriter(directory, config);
+
+        StringBuilder documentContents = new StringBuilder();
+        for(File f : ftList)
+        {
+            BufferedReader reader = new BufferedReader(new FileReader(f));
+            String line = null;
+
+            int documentNumber = 0;
+
+            ArrayList<StringBuilder> documentList = new ArrayList<>();
+
+            while ((line = reader.readLine()) != null)
+            {
+                if(line.contains("<DOC>"))
+                    documentContents = new StringBuilder();
+                else
+                {
+                    if(!line.contains("</DOC>"))
+                        documentContents.append(line);
+                    else {
+                        documentList.add(documentContents);
+                        String docNo = documentContents.substring((documentContents.indexOf("<DOCNO>") + 7), documentContents.indexOf("</DOCNO>")); // Number added to index is length of the tag
+                        String profile = documentContents.substring((documentContents.indexOf("<PROFILE>") + 9), documentContents.indexOf("</PROFILE>"));
+                        String date = documentContents.substring((documentContents.indexOf("<DATE>") + 6), documentContents.indexOf("</DATE>"));
+                        String headline = documentContents.substring((documentContents.indexOf("<HEADLINE>") + 10), documentContents.indexOf("</HEADLINE>"));
+                        String byline = null;
+                        if(documentContents.toString().contains("<BYLINE>"))
+                            byline = documentContents.substring((documentContents.indexOf("<BYLINE>") + 8), documentContents.indexOf("</BYLINE>"));
+                        else
+                            byline = "";
+                        String text = documentContents.substring((documentContents.indexOf("<TEXT>") + 6), documentContents.indexOf("</TEXT>"));
+                        String publisher = documentContents.substring((documentContents.indexOf("<PUB>") + 5), documentContents.indexOf("</PUB>"));
+                        String page = documentContents.substring((documentContents.indexOf("<PAGE>") + 6), documentContents.indexOf("</PAGE>"));
+
+                        // Creating the document
+                        Document doc = new Document();
+                        doc.add(new TextField("document-number", docNo, Field.Store.YES));
+                        doc.add(new TextField("profile", profile, Field.Store.YES));
+                        doc.add(new TextField("date", date, Field.Store.YES));
+                        doc.add(new TextField("headline", headline, Field.Store.YES));
+                        doc.add(new TextField("byline", byline, Field.Store.YES));
+                        doc.add(new TextField("text", text, Field.Store.YES));
+                        doc.add(new TextField("publisher", publisher, Field.Store.YES));
+                        doc.add(new TextField("page", page, Field.Store.YES));
+//
+                        // Save the document to the index
+                        iWriter.addDocument(doc);
+
+//                        System.out.println(docNo);
+//                        System.out.println(profile);
+//                        System.out.println(date);
+//                        System.out.println(headline);
+//                        System.out.println(byLine);
+//                        System.out.println(text);
+//                        System.out.println(publisher);
+//                        System.out.println(page);
+
+                    }
+                }
+            }
+        }
+
+        System.out.println("Index created.");
+
         // Commit changes and close everything
         iWriter.close();
         directory.close();
     }
 
 
-    public static void listDirectoryContents(File dir) {
-        File[] files = dir.listFiles();
-        for (File file : files) {
-            if (file.isDirectory()) {
-                System.out.println("directory:" + file.getName());
-                listDirectoryContents(file);
-            } else {
-                System.out.println("     file:" + file.getName());
-            }
-        }
-    }
     public static ArrayList<File> addFilesToList(File dir) {
 
         ArrayList<File> files = new ArrayList<>();
@@ -192,9 +158,6 @@ public class Index {
 
         while (iterator.hasNext())
             files.add((File) iterator.next());
-
-//        for(File f : files)
-//            System.out.println("FILE: " + f.getName());
 
         return files;
     }
